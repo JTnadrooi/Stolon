726bdbe7edfb9ed30698d7158d8271f1f9ba435a
JTnadrooi <jtnadrooi@gmail.com>
2025-04-15 13:50:58 +0200

    removed animation skip

--- src/Components/UI/UI.cs
+++ src/Components/UI/UI.cs
@@ -419,20 +419,20 @@ namespace Stolon
             int logoYoffset = 30;
             int menuLogoBoundingBoxClearing = 8;
 
-             if (milisecondsSinceStartup < 10000) // to skip start button click and animation
-            {
-                milisecondsSinceStartup = 10001;
-                menuDone = true;
-                menuRemoveTweener.Update(10);
+            // if (milisecondsSinceStartup < 10000) // to skip start button click and animation
+            //{
+            //    milisecondsSinceStartup = 10001;
+            //    menuDone = true;
+            //    menuRemoveTweener.Update(10);
 
-                Scene.MainInstance.SetBoard(new Player[]
-                        {
-                            new Player("player0"),
-                            new Player("player1"),
-                        });
-                Leave();
-                //startFrame = true;
-            }
+            //    Scene.MainInstance.SetBoard(new Player[]
+            //            {
+            //                new Player("player0"),
+            //                new Player("player1"),
+            //            });
+            //    Leave();
+            //    //startFrame = true;
+            //}
 
             #region inFlash
             menuLogoTileHider = new Rectangle(menuLogoDrawPos.ToPoint(), new Point((int)(menuLogoLines.Width * menuLogoScaling), (int)(rowHeight * menuLogoRowsHidden)));
