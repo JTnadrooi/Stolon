7e7dfacbee44f280702ea667e88b10549e397730
JTnadrooi <jtnadrooi@gmail.com>
2025-05-08 16:35:48 +0200

    refactored and improved performance of texture loading logic

--- src/ResourceManagement/ResourceCollection.cs
+++ src/ResourceManagement/ResourceCollection.cs
@@ -41,80 +41,67 @@ namespace Stolon
         public void UnLoad(string path);
         public void Add(TContent resource, string? newName = null);
     }
-    public class GameTextureCollection : IResourceCollection<GameTexture> 
+    public abstract class ResourceCollection<TContent> : IResourceCollection<TContent>
     {
+        protected readonly Dictionary<string, TContent> dictionary = new();
         private bool disposedValue;
-        private GameTexture pixel;
+
+        public ContentManager ContentManager { get; }
 
         public IEnumerable<string> Keys => dictionary.Keys;
-        public Dictionary<string, GameTexture> dictionary;
-        public IEnumerable<GameTexture> Values => dictionary.Values;
+        public IEnumerable<TContent> Values => dictionary.Values;
         public int Count => dictionary.Count;
-        public ContentManager ContentManager { get; }
-        public GameTexture this[string key] => GetReference(key);
+        public TContent this[string key] => GetReference(key);
 
-        public GameTextureCollection(ContentManager contentManager)
+        public ResourceCollection(ContentManager contentManager, Func<string, TContent?> loader)
         {
-            this.ContentManager = contentManager;
-            dictionary = new Dictionary<string, GameTexture>();
             string[] files = Directory.GetFiles(contentManager.RootDirectory, "*", SearchOption.AllDirectories);
             if (files.Length == 0) throw new Exception("No initial content found.");
+
             foreach (string file in files)
             {
                 Instance.DebugStream.WriteLine("[s]found file: " + file);
                 string toLoad = file[(contentManager.RootDirectory.Length + 1)..].Split('.')[..^1].ToJoinedString();
                 Instance.DebugStream.WriteLine("\tloading InTexture with id/key: " + toLoad);
-
-                try
-                {
-                    dictionary.Add(toLoad, new GameTexture(TexturePalette.Debug, contentManager.Load<Texture2D>(toLoad)));
-                    GameTexture inTexture = dictionary[toLoad];
-                    Color[] data = new Color[inTexture.Width * inTexture.Height];
-                    inTexture.GetColorData(data);
-                    for (int i = 0; i < data.Length; i++)
-                        if (!TexturePalette.Debug.Contains(data[i]) && data[i].A == 1)
-                        {
-                            Instance.DebugStream.WriteLine("found DEBUG texture: " + inTexture.Name);
-                            break;
-                        }
-                }
-                catch { }
+                TContent? loaderResult = loader(toLoad);
+                if (loaderResult != null) dictionary[toLoad] = loaderResult;
             }
-            pixel = new GameTexture(TexturePalette.Empty, new Texture2D(contentManager.GetGraphicsDevice(), 1, 1));
-            ((Texture2D)pixel).SetData(new Color[] { Color.White });
+            ContentManager = contentManager;
+
         }
-        public GameTexture Pixel => pixel;
-        public GameTexture GetReference(string path)
+
+        public virtual void Add(TContent resource, string? newName = null)
         {
-            GameTexture item = dictionary[path];
-            if (item.Palette == null) Instance.DebugStream.WriteLine("found corrupted/partial texture.");
-            return item;
-        }
-        public void Add(GameTexture resource, string? newName = null)
-        {
-            newName ??= resource.Name;
+            if (newName == null) throw new ArgumentNullException(nameof(newName));
             dictionary.Add(newName, resource);
         }
-        public TContent HardLoad<TContent>(string path)
+
+        public virtual TContent GetReference(string path)
         {
-            Instance.DebugStream.WriteLine("hardloading path: " + path);
-            return ContentManager.Load<TContent>(path);
+            if (!dictionary.TryGetValue(path, out var item))
+                throw new KeyNotFoundException($"Resource '{path}' not found.");
+            return item;
         }
-        public void UnLoad(string path)
+
+        public virtual void UnLoad(string path)
         {
-            dictionary[path].Dispose();
+            if (dictionary[path] is IDisposable disposable)
+                disposable.Dispose();
             dictionary.Remove(path);
         }
-        public void UnLoadAll()
+
+        public virtual void UnLoadAll()
         {
-            foreach (GameTexture texture in dictionary.Values) texture.Dispose();
+            foreach (var item in dictionary.Values)
+                if (item is IDisposable disposable)
+                    disposable.Dispose();
             dictionary.Clear();
-            pixel.Dispose();
         }
+
         public bool ContainsKey(string key) => dictionary.ContainsKey(key);
-        public bool TryGetValue(string key, [MaybeNullWhen(false)] out GameTexture value) => dictionary.TryGetValue(key, out value);
-        public IEnumerator<KeyValuePair<string, GameTexture>> GetEnumerator() => dictionary.GetEnumerator();
-        IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)dictionary).GetEnumerator();
+        public bool TryGetValue(string key, [MaybeNullWhen(false)] out TContent value) => dictionary.TryGetValue(key, out value);
+        public IEnumerator<KeyValuePair<string, TContent>> GetEnumerator() => dictionary.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
         protected virtual void Dispose(bool disposing)
         {
@@ -124,11 +111,59 @@ namespace Stolon
                 disposedValue = true;
             }
         }
-        ~GameTextureCollection() => Dispose(disposing: false);
+
+        ~ResourceCollection() => Dispose(disposing: false);
+
         public void Dispose()
         {
             Dispose(disposing: true);
             GC.SuppressFinalize(this);
+        }
+    }
+    public class GameTextureCollection : ResourceCollection<GameTexture>
+    {
+        private readonly GameTexture pixel;
+
+        public GameTexture Pixel => pixel;
+
+        public GameTextureCollection(ContentManager contentManager, bool debug = false) : base(contentManager, (toLoad) =>
+        {
+            try
+            {
+                GameTexture texture = new GameTexture(TexturePalette.Debug, contentManager.Load<Texture2D>(toLoad));
+                if(debug)
+                {
+                    Color[] data = new Color[texture.Width * texture.Height];
+                    texture.GetColorData(data);
+                    for (int i = 0; i < data.Length; i++)
+                    {
+                        if (!TexturePalette.Debug.Contains(data[i]) && data[i].A == 1)
+                        {
+                            Instance.DebugStream.WriteLine("found DEBUG texture: " + texture.Name);
+                            break;
+                        }
+                    }
+                }
+                return texture;
+            }
+            catch { return null; }
+        })
+        {
+            
+            pixel = new GameTexture(TexturePalette.Empty, new Texture2D(contentManager.GetGraphicsDevice(), 1, 1));
+            ((Texture2D)pixel).SetData(new[] { Color.White });
+        }
+
+        public TContent HardLoad<TContent>(string path)
+        {
+            Instance.DebugStream.WriteLine("hardloading path: " + path);
+            return ContentManager.Load<TContent>(path);
+        }
+
+        public override void UnLoadAll()
+        {
+            base.UnLoadAll();
+            pixel.Dispose();
         }
     }
 }
