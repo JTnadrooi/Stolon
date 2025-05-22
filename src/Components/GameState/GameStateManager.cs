400d7d5e19935c15593913dd09284a954b9686bc
JTnadrooi <jtnadrooi@gmail.com>
2025-05-22 11:25:15 +0200

    added helper functions

--- src/Components/GameState/GameStateManager.cs
+++ src/Components/GameState/GameStateManager.cs
@@ -20,27 +20,46 @@ namespace Stolon
 {
     public interface IGameState
     {
-        void Update(int elapsedMiliseconds);
-        void Draw(SpriteBatch spriteBatch);
+        public void Update(int elapsedMilliseconds);
+        public void Draw(SpriteBatch spriteBatch);
+        public string DRPStatus { get; }
     }
 
+    public static class GameStateExtensions
+    {
+        public static string GetID(this IGameState state) => GameStateHelpers.GetID(state.GetType());
+    }
+    public static class GameStateHelpers
+    {
+        public static string GetID<T>() where T : IGameState, new() => GetID(typeof(T));
+        public static string GetID(Type type) => type.FullName ?? throw new Exception();
+    }
     public class GameStateManager
     {
         private IGameState? _currentState;
+        private readonly Dictionary<string, IGameState> _stateMemory;
 
-        public void ChangeState(IGameState newState)
+        public IGameState Current => _currentState ?? throw new Exception();
+
+        public GameStateManager()
         {
-            _currentState = newState;
+            _stateMemory = new Dictionary<string, IGameState>();
         }
 
-        public void Update(int elapsedMiliseconds)
+        public void ChangeState<T>(bool @override = false) where T : IGameState, new()
         {
-            _currentState?.Update(elapsedMiliseconds);
+            if (@override) _currentState = _stateMemory[GameStateHelpers.GetID<T>()] = new T();
+            else _currentState = _stateMemory[GameStateHelpers.GetID<T>()] = _stateMemory.GetValueOrDefault(GameStateHelpers.GetID<T>()) ?? new T();
+        }
+
+        public void Update(int elapsedMilliseconds)
+        {
+            _currentState.Update(elapsedMilliseconds);
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            _currentState?.Draw(spriteBatch);
+            _currentState.Draw(spriteBatch);
         }
     }
 }
